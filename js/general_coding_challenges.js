00cd905857e41af4b78798c3be7d9d32ee261b51
Justin McCaleb <justinleemccaleb@gmail.com>
2020-05-12 16:59:46 -0500

    adding general coding challenges docs

---
+++ js/general_coding_challenges.js
@@ -0,0 +1,23 @@
+"use strict";
+
+(function($) {
+    $(document).ready(function() {
+        alert( 'The DOM has finished loading!' );
+    });
+    /*
+    A narcissistic number is a non-negative integer (n) with m digits where the sum of all the individual digits raised to the power m is equal to n.
+
+        For example:
+            If n is 153, then m (the number of digits) is 3 and:
+
+            1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+
+            So, 153 is a narcissistic number.
+
+            Objective: Write a script to generate and output the first 25 narcissistic integers.
+     */
+
+
+
+
+})(jQuery);
